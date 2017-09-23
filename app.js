2e1626e745c40f9028a5854af7dbc8545c508641
Amrit <ehthind@gmail.com>
2017-09-22 18:57:58 -0700

    image rename, added fav icon.

--- app.js
+++ app.js
@@ -35,8 +35,7 @@ app.engine('hbs', hbs({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'assets', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
