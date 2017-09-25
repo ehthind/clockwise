5e5f183bba0c83b6eb438bfc431e08b7ad7607f0
Amrit <ehthind@gmail.com>
2017-09-25 14:07:37 -0700

    error logging on production

--- app.js
+++ app.js
@@ -121,9 +121,10 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get('env') === 'development' ? err : err;
 
     // render the error page
+    console.log('IN ERROR MESSAGE');
     console.log(err.message);
     res.status(err.status || 500);
     res.render('error');
