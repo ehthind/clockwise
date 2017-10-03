ec915eb3cc821766333979d8939e30c2afcc2be4
Amrit <ehthind@gmail.com>
2017-10-02 20:17:03 -0700

    save service

--- app/services/save.service.js
+++ app/services/save.service.js
@@ -5,139 +5,168 @@
         .module('app')
         .service('saveService', saveService);
 
-    function saveService($http, $rootScope, eventService) {
+    function saveService($rootScope, $http, eventService) {
 
+        // Public functions
         this.loadSavedSchedules = loadSavedSchedules;
         this.loadSavedCourses = loadSavedCourses;
-        this.saveSchedule = saveSchedule;        
+        this.saveSchedule = saveSchedule;
 
         this.getSavedSchedules = getSavedSchedules;
         this.getSavedCourses = getSavedCourses;
 
-        var saved_schedules = [];
-        var saved_courses = [];
+        // Globals
+        var savedSchedule = [];
+        var savedCourses = [];
         var colorIndex = 0;
 
-        const schedulesUrl = "/api/databaseAPI/savedSchedules";
-        const classesUrl = "/api/databaseAPI/savedCourses";
+        // API end points
+        const scheduleEndPoint = "/api/databaseAPI/savedSchedules";
+        const courseEndPoint = "/api/databaseAPI/savedCourses";
 
         ////////////////
 
+
+
+        /**
+         * Invokes a get request which returns all saved schedules
+         * for the logged in user. Updates 'savedSchedule' with
+         * get result data.
+         */
+
         function loadSavedSchedules() {
 
-            fetchSavedSchedules().then((schedules) => {
+            getRequest({
+
+                url: scheduleEndPoint,
+                params: {}
+
+            }).then(schedules => {
 
                 // empty the array.
-                saved_schedules.length = 0
+                savedSchedule.length = 0
 
-                schedules.forEach((schedule) => {
-                    saved_schedules.push(schedule)
+                schedules.forEach(schedule => {
+                    savedSchedule.push(schedule)
                 }, this);
 
+            }).catch(err => {
+                console.error(err)
             });
         }
 
-        function loadSavedCourses(courseId) {
 
-            return (fetchSavedCourses(courseId).then((courses) => {
 
-                // empty the array.
-                saved_courses.length = 0
+        /**
+         * Invokes a get request which returns all saved courses
+         * for a particular schedule. Updates 'savedCourses' accordingly
+         * 
+         * @param {Number} scheduleId 
+         */
 
-                courses.forEach((course) => {
-                    saved_courses.push(course)
+        function loadSavedCourses(scheduleId) {
+
+            return (getRequest({
+                
+                'url': courseEndPoint,
+                'params': {
+                    'scheduleId': scheduleId
+                }
+
+            }).then((courses) => {
+                // Empty the array.
+                savedCourses.length = 0
+
+                courses.forEach(course => {
+                    savedCourses.push(course)
                 }, this);
+
                 return (courses)
 
+            }).catch(err => {
+                console.error(err)
             }));
         }
 
-        function saveSchedule(name) {
-            insertSchedule(name).then((insertId) => {
-                let eventList = eventService.getUniqueCrns();
 
-                eventList.forEach(function (course) {
-                    if (course.schedule_type === 'Lecture' || course.schedule_type === 'Lecture Topic' || course.schedule_type === 'Gradable Lab') {
 
-                        let insertData = {
-                            'scheduleId': insertId,
-                            'courseId': course.courseID,
-                            'lec_crn': course.crn,
-                            'lab_crn': null
-                        }
+        /**
+         * Invokes a post request which will save the state of
+         * of the current schedule for the logged in user. Accesses
+         * the current state of events via 'eventService' Preforms
+         * data validation before posting.
+         * 
+         * @param {String} scheduleName 
+         */
 
-                        for (var index = 0; index < eventList.length; index++) {
-                            var lab = eventList[index];
-                            if (course.courseID === lab.courseID && course.crn !== lab.crn) {
-                                insertData.lab_crn = lab.crn;
-                            }
-                        }
-                        
-                        insertCourse(insertData).then((response) => {
-                            console.log(response);
-                        });
+        function saveSchedule(scheduleName) {
+
+            postRequest({
+                'url': scheduleEndPoint,
+                'params': {
+                    'name': scheduleName,
+                    'term': $rootScope.term.val
+                }
+
+            }).then((insertId) => {
+            
+                saveState(insertId)
+            
+            }).catch(err => {
+                console.error(err)
+            });
+        }
+
+        const saveState = (insertId) => {
+            
+            let eventList = eventService.getUniqueCrns();
+
+            eventList.forEach(function (course) {
+                // Ensure course is one of 3 allowed lecture types.
+                if (course.schedule_type === 'Lecture' || course.schedule_type === 'Lecture Topic' || course.schedule_type === 'Gradable Lab') {
+
+                    let postData = {
+                        'scheduleId': insertId,
+                        'courseId': course.courseID,
+                        'lec_crn': course.crn,
+                        'lab_crn': null // incase course has no lab section.
                     }
-                }, this);
-                console.log(eventList);
-                console.log(insertId);
+
+                    for (var index = 0; index < eventList.length; index++) {
+                        var lab = eventList[index];
+                        // Ensure current event type is a 'lab'.
+                        if (course.courseID === lab.courseID && course.crn !== lab.crn) {
+                            postData.lab_crn = lab.crn;
+                        }
+                    }
+
+                    postRequest({
+                        url: courseEndPoint,
+                        params: postData
+                    })
+                }
             });
         }
 
 
 
         // Getters //
+
         function getSavedSchedules() {
-            return saved_schedules;
+            return savedSchedule;
         }
 
         function getSavedCourses() {
-            return saved_courses;
-        }
-        
-        // Helper functions //
-
-        var insertCourse = (course) => {
-
-            return $http.post(classesUrl, course).then((response) => {
-                return response.data;
-            });
-
+            return savedCourses;
         }
 
-        var insertSchedule = (name) => {
-            var data = {
-                'name': name,
-                'term': $rootScope.term.val
-            };
 
-            return $http.post(schedulesUrl, data).then((response) => {
-                return response.data;
-            });
+        // Request generators //
 
-        }
+        const getRequest = options =>
+            $http.get(options.url, options.params).then(response => response.data).catch(err => console.error(err))
 
-        var fetchSavedCourses = (scheduleId) => {
-            var data = {
-                params: {
-                    'scheduleId': scheduleId
-                }
-            };
-
-            return $http.get(classesUrl, data).then((response) => {
-                return response.data;
-            });
-        }
-
-        var fetchSavedSchedules = () => {
-            var data = {
-                params: {
-                    'userId': 79
-                }
-            };
-
-            return $http.get(schedulesUrl, data).then((response) => {
-                return response.data;
-            });
-        }
+        const postRequest = options =>
+            $http.post(options.url, options.params).then(response => response.data).catch(err => console.error(err))
     }
 })();
